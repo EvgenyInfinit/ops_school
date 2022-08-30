91b1c618d8a43517065add7c82cb9884c7c684b8
gotech <gotech@gotech.local>
2022-08-30 15:11:14 +0300

    test3

--- materials/vagrant/fixScripts/exercise5-fix_server1.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
 #add fix to exercise5-server1 here
+
+sudo sed -i 's/#   StrictHostKeyChecking ask/StrictHostKeyChecking no/g' /etc/ssh/ssh_config
+# 
+ssh-keygen -t rsa -b 4096 -N ""  -C "server1 ssh key" -f  /home/vagrant/.ssh/id_rsa
+chown vagrant:vagrant /home/vagrant/.ssh/id_rsa 
+chown vagrant:vagrant /home/vagrant/.ssh/id_rsa.pub
+#
+cp /home/vagrant/.ssh/id_rsa ~/.ssh/id_rsa
+chmod 600 ~/.ssh/id_rsa
+cp /home/vagrant/.ssh/id_rsa.pub ~/.ssh/id_rsa.pub
+chmod 644 ~/.ssh/id_rsa.pub
+#
+cat /vagrant/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys
+# sheared mount
+cp /home/vagrant/.ssh/id_rsa.pub /vagrant/
--- materials/vagrant/fixScripts/exercise5-fix_server2.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server2.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 #add fix to exercise5-server2 here
+sudo sed -i 's/#   StrictHostKeyChecking ask/StrictHostKeyChecking no/g' /etc/ssh/ssh_config
+
+cat /vagrant/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys 
+
+cp /vagrant/id_rsa.pub /home/vagrant/.ssh/id_rsa.pub
+chown vagrant:vagrant /home/vagrant/.ssh/id_rsa.pub
+chmod 644 /home/vagrant/.ssh/id_rsa.pub
+
+cp /vagrant/id_rsa.pub ~/.ssh/id_rsa.pub
+chmod 644 ~/.ssh/id_rsa.pub
